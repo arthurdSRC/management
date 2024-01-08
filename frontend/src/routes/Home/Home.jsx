f03a5ebbf478aec60c7862f387b61e704fcf100a
arthur <arthur.souza.reis222@gmail.com>
2024-01-08 15:27:39 -0300

    criando e estilizando home

--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import "./Home.css"
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <main className='home-container'>
+      <div className="container home">
+
+        <h2>Suas Festas</h2>
+
+        <section className='party-container'>
+          {/* <div className="party">
+            <img src="https://as2.ftcdn.net/v2/jpg/01/20/28/25/1000_F_120282530_gMCruc8XX2mwf5YtODLV2O1TGHzu4CAb.jpg" alt="#" />
+            <p>Festa de Aniversário</p>
+            <button className="btn-details">Detalhes</button>
+          </div> */}
+        </section>
+      </div>
+    </main>
   )
 }
